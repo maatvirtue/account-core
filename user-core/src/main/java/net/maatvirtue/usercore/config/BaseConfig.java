c0a6b4f8603d1daec78add2258581c13100ea765
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2016-07-23 22:04:25 -0400

    updated to use spring cloud config

--- user-core/src/main/java/net/maatvirtue/usercore/config/BaseConfig.java
+++ user-core/src/main/java/net/maatvirtue/usercore/config/BaseConfig.java
@@ -1,31 +1,12 @@
 package net.maatvirtue.usercore.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class BaseConfig
 {
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer()
-	{
-		return new PropertySourcesPlaceholderConfigurer();
-	}
-
-	@Bean
-	public static ConfigCorePropertySource configCorePropertySource(ConfigurableEnvironment configurableEnvironment, @Value("${core.config.url}") String configCoreUrl)
-	{
-		ConfigCorePropertySource configCorePropertySource = new ConfigCorePropertySource("config-core", configCoreUrl);
-
-		configurableEnvironment.getPropertySources().addLast(configCorePropertySource);
-
-		return configCorePropertySource;
-	}
-
 	@Bean
 	public LocalValidatorFactoryBean validator()
 	{
