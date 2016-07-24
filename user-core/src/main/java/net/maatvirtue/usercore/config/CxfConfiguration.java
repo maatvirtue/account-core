62c4257cf23cc7979d147a1eba9da30005c10481
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2016-07-23 23:15:46 -0400

    fixed CXF config

--- user-core/src/main/java/net/maatvirtue/usercore/config/CxfConfiguration.java
+++ user-core/src/main/java/net/maatvirtue/usercore/config/CxfConfiguration.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import net.maatvirtue.wsutils.filter.CorsFilter;
 import net.maatvirtue.wsutils.filter.RequestLogFilter;
 import net.maatvirtue.wsutils.restexception.providers.RestExceptionFeature;
-import org.apache.cxf.jaxrs.spring.SpringComponentScanServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(SpringComponentScanServer.class)
 public class CxfConfiguration
 {
 	@Bean
