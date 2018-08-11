32af111c6ba84fcd6e2702469f2e2d8bf182ffb2
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2018-08-11 15:13:08 -0400

    removed consul service discoveries and added authentication to config-ws

--- user-ws/src/main/java/net/nlacombe/userws/UserWsApplication.java
+++ user-ws/src/main/java/net/nlacombe/userws/UserWsApplication.java
@@ -2,10 +2,8 @@ package net.nlacombe.userws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserWsApplication
 {
 	public static void main(String[] args)
